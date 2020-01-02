68d8774ec7b14d6dfcaa0a598e6e5cff6fe8e206
huy-xu <nhathuyhedspi@gmail.com>
2020-01-02 17:06:03 +0700

    Create front-end

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav className="flex justify-end">
+            <p className="f3 link dim black pa3">Sign Out</p>
+        </nav>
+    )
+};
+
+export default Navigation;
