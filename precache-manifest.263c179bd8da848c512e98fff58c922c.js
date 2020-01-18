b71fdc76deca71bd9006f4f3e9860697e80c0499
huy-xu <nhathuyhedspi@gmail.com>
2020-01-19 00:41:42 +0700

    Updates

---
+++ precache-manifest.263c179bd8da848c512e98fff58c922c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72055febca817caf680cd07c9fa1fb8e",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "b83a3cb27dbff48bf726",
+    "url": "/face-recognition-brain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e07a1078abf7445361a0",
+    "url": "/face-recognition-brain/static/css/main.b6557d82.chunk.css"
+  },
+  {
+    "revision": "b83a3cb27dbff48bf726",
+    "url": "/face-recognition-brain/static/js/2.77121c4b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/face-recognition-brain/static/js/2.77121c4b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e07a1078abf7445361a0",
+    "url": "/face-recognition-brain/static/js/main.acc634bb.chunk.js"
+  },
+  {
+    "revision": "cd099675855a957083bb",
+    "url": "/face-recognition-brain/static/js/runtime-main.b72ae444.js"
+  }
+]);
